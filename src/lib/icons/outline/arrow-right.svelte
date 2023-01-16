363c4e526d664f4d7ef9a40b46f6cfa7ac20837f
Bitlime <contact@lorenzosuffritti.com>
2023-01-16 01:28:11 +0100

    Icons size updated
    Icon size can be passed as property now

--- src/lib/icons/outline/arrow-right.svelte
+++ src/lib/icons/outline/arrow-right.svelte
@@ -1,3 +1,3 @@
-<svg class="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+<svg class="h-full w-full" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
 <path d="M13.5 4.5L21 12M21 12L13.5 19.5M21 12H3" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
--- src/lib/icons/outline/chevron-double-left.svelte
+++ src/lib/icons/outline/chevron-double-left.svelte
@@ -1,3 +1,3 @@
-<svg class="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+<svg class="h-full w-full" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
 <path d="M18.75 19.5L11.25 12L18.75 4.5M12.75 19.5L5.25 12L12.75 4.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
