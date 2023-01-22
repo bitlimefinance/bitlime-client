750f412563d08cf8e7b52607c2e049c2bfcd97dc
Bitlime <contact@lorenzosuffritti.com>
2023-01-22 22:15:47 +0100

    Passworder

--- src/lib/blw/components/createWallet.svelte
+++ src/lib/blw/components/createWallet.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
 	import Button from "$lib/components/general/button.svelte";
+	import Icon from "$lib/components/general/icon.svelte";
     import Input from "$lib/components/general/input.svelte";
-    import passworder from "@metamask/browser-passworder";
-	import assert from "assert";
+	import Tooltip from "$lib/components/general/tooltip.svelte";
+	import { debug, debugError } from "$lib/core/utils/debug";
+	import { decryptCipherText, encryptMessage, type EncryptedVault } from "$lib/core/utils/passworder";
+	import checkPasswordStrength, { PasswordStrengthLevels, type PasswordStrength } from "$lib/core/utils/passwordStrength";
 
     let step: 1 | 2 | 3 = 1;
 
     let password: string;
     let confirmPassword: string;
+    let passworMatch: boolean;
+    let passwordStrength: PasswordStrength;
 
-    let cipher: string;
+    const checkPassword = async () => {
+        if(password) passwordStrength = await checkPasswordStrength(password);
+        if (password && confirmPassword) {
+            console.log(passwordStrength);
+            
+            if (password !== confirmPassword) {
+                passworMatch = false;
+            } else {
+                passworMatch = true;
+            }
+        } else {
+            passworMatch = false;
+        }
+    }
+
+    $: password, confirmPassword, checkPassword(); 
 
 </script>
 
@@ -30,15 +50,47 @@
             <div class="untheme w-full border-b {step===3?"border-green-400":""}"/>
         </div>
         
-
+        <!-- steps -->
         {#if step===1}
         <div class="space-y-3">
+            <div class="text-sm opacity-70">
+                This password will be used to secure your wallet, so make sure it's strong and keep it safe.
+            </div>
             <Input
                 label="Password"
                 type="password"
                 id="blw-password"
                 bind:value={password}
             />
+            <!-- password strength -->
+            {#if password}
+                <div class="flex justify-between items-center text-sm mb-2 pb-2 border-b">
+                    <Tooltip content="Must be at least <span class='font-semibold'>'Good'</span>, but we strongly recommend <span class='font-semibold'>'Very Strong'</span>" title="Password Strength">
+                        <div class="flex justify-start gap-2 cursor-default">
+                            <span class="opacity-70">Strength:</span>
+                            {#if passwordStrength===PasswordStrengthLevels.Short}
+                                <span class="text-red-400">Too {PasswordStrengthLevels.Short.toLowerCase()}</span>
+                            {:else if passwordStrength===PasswordStrengthLevels.Common}
+                                <span class="text-yellow-400">Too {PasswordStrengthLevels.Common.toLowerCase()}</span>
+                            {:else if passwordStrength===PasswordStrengthLevels.Weak}
+                                <span class="text-yellow-400">Too {PasswordStrengthLevels.Weak.toLowerCase()}</span>
+                            {:else if passwordStrength===PasswordStrengthLevels.Good}
+                                <span class="text-green-400">{PasswordStrengthLevels.Good}</span>
+                            {:else if passwordStrength===PasswordStrengthLevels.Strong}
+                                <span class="text-green-400">{PasswordStrengthLevels.Strong}</span>
+                            {:else if passwordStrength===PasswordStrengthLevels.VeryStrong}
+                                <span class="text-green-400">{PasswordStrengthLevels.VeryStrong}</span>
+                            {/if}
+                        </div>
+                    </Tooltip>
+                    <!-- {#if passwordStrength===PasswordStrengthLevels.VeryStrong || passwordStrength===PasswordStrengthLevels.Strong || passwordStrength===PasswordStrengthLevels.Good}
+                        <Icon icon="check" color="rgb(49, 196, 141)"/>
+                    {:else}
+                        <Icon icon="x-mark" color="red"/>
+                    {/if} -->
+                </div>
+            {/if}
+
             <Input
                 label="Confirm Password"
                 type="password"
@@ -47,15 +99,21 @@
             />
             <Button
                 label="Create"
-                on:click={() => {
-                    passworder
-                    .encrypt(password, { test: "test" })
-                    .then((res) => {
-                        cipher = res;
-                    })
+                disabled={!passworMatch}
+                on:click={async () => {
+                    try {
+                        let vault = await encryptMessage('test', password);
+                        debug(await checkPasswordStrength(password))
+                        debug(vault);
+                        debug(await decryptCipherText(vault, password));
+                        step = 2;
+                    } catch (error) {
+                        debugError(error);
+                        window.alert("Sorry, something went wrong. Please try again.");
+                    }
+
                 }}
             />
-            {cipher}
         </div>
         {:else if step===2}
         <div></div>
---
+++ src/lib/core/utils/passwordStrength.ts
@@ -0,0 +1,64 @@
+// Password strengths
+const enum PasswordStrengthLevels {
+    Short = "Short",
+    Common = "Common",
+    Weak = "Weak",
+    Good = "Good",
+    Strong = "Strong",
+    VeryStrong = "Very strong"
+};
+
+type PasswordStrength = PasswordStrengthLevels;
+
+
+// Expected length of all passwords
+const DefaultMinimumLength = 8;
+
+// Regex to check for a common password string - all based on 5+ length passwords
+const commonPasswordPatterns = /passw.*|12345.*|09876.*|qwert.*|asdfg.*|zxcvb.*|footb.*|baseb.*|drago.*/;
+
+//
+// Checks if the given password matches a set of common password
+//
+async function isPasswordCommon(password: string): Promise<boolean> {
+    return commonPasswordPatterns.test(password);
+}
+
+//
+// Returns the strength of the current password
+//
+async function checkPasswordStrength(password: string, minimumLength?: number): Promise<PasswordStrength> {
+
+    // Build up the strenth of our password
+    let numberOfElements = 0;
+    numberOfElements = /.*[a-z].*/.test(password) ? ++numberOfElements : numberOfElements;      // Lowercase letters
+    numberOfElements = /.*[A-Z].*/.test(password) ? ++numberOfElements : numberOfElements;      // Uppercase letters
+    numberOfElements = /.*[0-9].*/.test(password) ? ++numberOfElements : numberOfElements;      // Numbers
+    numberOfElements = /[^a-zA-Z0-9]/.test(password) ? ++numberOfElements : numberOfElements;   // Special characters (inc. space)
+
+    // Assume we have a poor password already
+    let currentPasswordStrength = PasswordStrengthLevels.Short;
+
+    // Check then strenth of this password using some simple rules
+    if (password === null || password.length < (minimumLength || DefaultMinimumLength)) {
+        currentPasswordStrength = PasswordStrengthLevels.Short;
+    } else if (await isPasswordCommon(password) === true) {
+        currentPasswordStrength = PasswordStrengthLevels.Common;
+    } else if (numberOfElements === 0 || numberOfElements === 1 || numberOfElements === 2) {
+        currentPasswordStrength = PasswordStrengthLevels.Weak;
+    } else if (numberOfElements === 3 || password.length<10) {
+        currentPasswordStrength = PasswordStrengthLevels.Good;
+    } else if(password.length<12) {
+        currentPasswordStrength = PasswordStrengthLevels.Strong;
+    } else {
+        currentPasswordStrength = PasswordStrengthLevels.VeryStrong;
+    }
+
+    // Return the strength of this password
+    return currentPasswordStrength;
+}
+
+
+export default checkPasswordStrength;
+export { PasswordStrengthLevels };
+export type { PasswordStrength };
